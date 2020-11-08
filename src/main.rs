6918f94c084e0a7d21c741a38ffa5e88f32496c7
Elijah Voigt <elijah.caine.mv@gmail.com>
2020-11-08 08:37:30 -0800

    Step 2: Draw a Seagl
    In this step we add our first Component: Seagl.
    
    Then we added two entities to our game: Camera and Seagl.
    
    Both entities used the Transform state, which required us to add the
    Transform bundle to the game.
    
    We also loaded a sprite sheet and attached our first Sprite to our Seagl entity.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use amethyst::{
+    assets::{AssetStorage, Loader},
+    core::transform::{Transform, TransformBundle},
+    ecs::{Component, DenseVecStorage},
     prelude::*,
     renderer::{
         plugins::{RenderFlat2D, RenderToWindow},
         types::DefaultBackend,
         RenderingBundle,
     },
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
     utils::application_root_dir,
 };
 
@@ -26,6 +30,10 @@ fn main() -> amethyst::Result<()> {
     // For now it's a simple 500x500 pixel window, but you can make it whatever you want!
     let display_config_path = app_root.join("config").join("display.ron");
 
+    // The Transform bundle allows us to use the Transform component.
+    // This is used to tag entities with positions in space and move then around.
+    let transform = TransformBundle::new();
+
     // Our Rendering bundle draws a simple orange backdrop and sets some defaults for a 2D game.
     let renderer = RenderingBundle::<DefaultBackend>::new()
         .with_plugin(
@@ -35,7 +43,9 @@ fn main() -> amethyst::Result<()> {
         .with_plugin(RenderFlat2D::default());
 
     // Create our game object with our bundles.
-    let game_data = GameDataBuilder::default().with_bundle(renderer)?;
+    let game_data = GameDataBuilder::default()
+        .with_bundle(renderer)?
+        .with_bundle(transform)?;
 
     // Create our application with our assets, our starting state, and our systems.
     let mut game = Application::new(assets_dir, SeaglState, game_data)?;
@@ -52,6 +62,101 @@ fn main() -> amethyst::Result<()> {
 // https://book.amethyst.rs/stable/concepts/state.html
 struct SeaglState;
 
-// In the next step we will implement custom State logic, but for now we can use the out of the box
-// implementation provided by Amethyst to get our game to compile.
-impl SimpleState for SeaglState {}
+impl SimpleState for SeaglState {
+    // Here we've implemented one of the `SimpleState` methods: `on_start`.
+    // When the State is first booted up Amethyst will run through this code _once_.
+    fn on_start(&mut self, data: StateData<GameData>) {
+        // We don't have any Systems that use Seal yet, so we need to manually register this
+        // component with the world.
+        data.world.register::<Seagl>();
+
+        // Create our Camera
+        // Our Camera won't move around, we still need to initialize it in space, so we add the
+        // transform component to our Camera entity.
+        let mut transform = Transform::default();
+
+        // We set the Z transform to `1` so we can later move sprites to different Z levels.
+        // If everything was at the same Z level sprites would overlap at random -- CHAOS!
+        transform.set_translation_xyz(50.0, 50.0, 1.0);
+
+        // Create an entity with the Camera component.
+        // Now we can place entities in front of or behind the camera.
+        data.world
+            .create_entity()
+            .with(Camera::standard_2d(100.0, 100.0))
+            .with(transform)
+            .build();
+
+        // Next we're going to load up our sprite sheet so we have something to represent our Seagl entity.
+        let sprite_sheet_handle = {
+            // Create a loader object which we will use at the end to load our final sprite sheet.
+            let loader = data.world.read_resource::<Loader>();
+
+            // Create a storage object
+            let texture_storage = data.world.read_resource::<AssetStorage<Texture>>();
+
+            // First we have to load a texture which represents the entire PNG image.
+            let texture_handle = loader.load(
+                "texture/spritesheet.png",
+                ImageFormat::default(),
+                (),
+                &texture_storage,
+            );
+
+            // Then we need to create a sprite sheet storage object
+            let sprite_sheet_store = data.world.read_resource::<AssetStorage<SpriteSheet>>();
+
+            // Convert our massive image into a sprite sheet based on the mapping in
+            // `spritesheet.ron`.
+            //
+            // A Sprite Sheet is like a Texture, but you can index it like an array.
+            // in our `spritesheet.ron` file we map areas of the texture to specific sprites.
+            // This means we can have sprites overlap and pack tightly.
+            // Later we will get the first element from this sheet, the Seagl.
+            //
+            // One last thing: This is the last statement in a let foo = { ... }; block, so it is
+            // returned the value to be assigned to `sprite_sheet_handle`.
+            loader.load(
+                "texture/spritesheet.ron",
+                SpriteSheetFormat(texture_handle),
+                (),
+                &sprite_sheet_store,
+            )
+        };
+
+        // Next we create a Seagl.
+        // It definitely lives in space, and will move around, so we give it a transform component.
+        let mut transform = Transform::default();
+
+        // Our arena is 100x100, so we draw (see our Camera component above)
+        // So to draw our Seagl at the center we put it at 50x50
+        transform.set_translation_xyz(50.0, 50.0, 0.0);
+
+        // Using our sprite sheet, get the first sprite specified by `spritesheet.ron`
+        let sprite = SpriteRender::new(sprite_sheet_handle.clone(), 0);
+
+        // Create a Seagl struct, which happens to just be a marker on this entity.
+        let seagl = Seagl::default();
+
+        // Build our main player entity
+        data.world
+            .create_entity()
+            .with(seagl)
+            .with(sprite)
+            .with(transform)
+            .build();
+    }
+}
+
+// Our Seagl component.
+// We use a zero-size struct as a marker on an entity later in the exercise.
+// We can filter on, for example, all entities with Seagl and Transform components
+// and do some transformation on all of them.
+#[derive(Default)]
+pub struct Seagl;
+
+// This is boilerplate Amethyst.
+// We could optimize the Storage type if we wanted, but DenseVecStorage is a pretty good default.
+impl Component for Seagl {
+    type Storage = DenseVecStorage<Self>;
+}
